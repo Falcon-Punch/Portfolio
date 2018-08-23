869686a5d9fd72f62b2802dc73074138b26c20cb
Joseph Schell <JosephSchell510@gmail.com>
2018-08-22 23:30:31 -0700

    Added 6 placeholder items under portfolio section. Animations on image hovering has been added.

--- js/script.js
+++ js/script.js
@@ -63,5 +63,18 @@ $(document).ready(function()
 		}
 	});
 
+	$("[data-fancybox]").fancybox();
+
+	$(".items").isotope(
+	{
+		filter: '*',
+		animationOptions:
+		{
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+	})
+
 });
 
